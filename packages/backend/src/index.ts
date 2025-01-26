ac2a224f656675ac690518c3f26053a8c2873d43
Christian Frenzl <chriseff@users.noreply.github.com>
2025-01-26 20:12:33 +0100

    save history in MongoDB

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,19 +1,39 @@
+import 'dotenv/config'
 import express from 'express'
 import { Server } from 'socket.io'
 
+import { MongoClient, ServerApiVersion } from 'mongodb'
+
 import evaluateExpression from './evaluateExpression'
+
+if (!process.env.MONGODB_URI) {
+	throw new Error('MONGODB_URI not set. Please set up a valid .env file.')
+}
+if (!process.env.WEBSOCKET_PORT) {
+	throw new Error('WEBSOCKET_PORT not set. Please set up a valid .env file.')
+}
+
+const mongoClient = new MongoClient(process.env.MONGODB_URI, {
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
+})
+await mongoClient.connect()
+const mongoPingResponse = await mongoClient.db('admin').command({ ping: 1 })
+if (!mongoPingResponse.ok) {
+	throw new Error('Could not connect to MongoDB. Please check your connection string.')
+} else {
+	console.log('Successfully connected to MongoDB.')
+}
 
 const app = express()
 
-const PORT = 3000
-
 app.use(express.json())
 
-// TODO: history should be stored in DB
-let history: string[] = []
-
-const expressServer = app.listen(PORT, () => {
-	console.log('Server is running on http://localhost:' + PORT)
+const expressServer = app.listen(process.env.WEBSOCKET_PORT, () => {
+	console.log('Server is running on http://localhost:' + process.env.WEBSOCKET_PORT)
 })
 
 const io = new Server(expressServer, {
@@ -30,24 +50,42 @@ io.on('connection', socket => {
 	socket.emit('bot_message', 'For a history of the last problems I solved for you (up to 10), just type "history".')
 	socket.emit('bot_message', 'To send your message, you can either press the enter key or click the "Send" button.')
 
-	socket.on('user_message', (message: string) => {
+	socket.on('user_message', async (message: string) => {
 		if (message === 'history') {
-			if (history.length === 0) {
-				socket.emit('bot_message', "It seems I haven't solved any problems for you yet.")
-				return
+			try {
+				// To get the latest 10 entries, we have to query by "newest first",
+				// but since we want to display them in chronological order, we then need to reverse the array.
+				const history = (
+					await mongoClient.db('calcubot').collection('history').find().sort({ timestamp: -1 }).limit(10).toArray()
+				).reverse()
+
+				if (history.length === 0) {
+					socket.emit('bot_message', "It seems I haven't solved any problems for you yet.")
+					return
+				}
+				socket.emit('bot_message', 'Here are the last problems I solved for you:')
+
+				history.forEach(entry => {
+					socket.emit('bot_message', entry.problem)
+				})
+			} catch (error) {
+				socket.emit('bot_message', 'Sorry, something went wrong while fetching the history from my database.')
 			}
-			socket.emit('bot_message', 'Here are the last problems I solved for you:')
-			history.forEach(solvedProblem => {
-				socket.emit('bot_message', solvedProblem)
-			})
 			return
 		}
 
 		try {
 			const result = evaluateExpression(message)
 			const solvedProblem = `${message} = ${result}`
-			history = [...history.slice(-9), solvedProblem]
 			socket.emit('bot_message', solvedProblem)
+			try {
+				mongoClient.db('calcubot').collection('history').insertOne({ problem: solvedProblem, timestamp: new Date() })
+			} catch (error) {
+				socket.emit(
+					'bot_message',
+					`Sorry, something went wrong while saving "${solvedProblem}" to my database, so it won't show up in the history.`,
+				)
+			}
 		} catch (error) {
 			socket.emit(
 				'bot_message',
